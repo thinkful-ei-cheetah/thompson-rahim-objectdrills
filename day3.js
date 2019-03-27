c116eef7598c9765130625e075d99540dc091d63
Thompson Galetovic <200conceptz@gmail.com>
2019-03-27 16:00:22 -0400

    boss man

--- day3.js
+++ day3.js
@@ -20,3 +20,21 @@ const loafer = {
 for (let prop in loafer) {
   console.log(`${prop} : ${loafer[prop]}`);
 }
+
+const Hobbit =  {height: 'short', feet: 'smelly', meals: ['breakfast', 'second breakfast', 'elevenses', 'lunch', 'afternoon tea', 'dinner', 'supper'] };
+//console.log(Hobbit.meals[3]);
+
+const Friends = [
+  {name: 'Rahim', jobTitle: 'Cable cutter', },
+  {name: 'Chris', jobTitle: 'Booty grabber', boss: 'Nobody'},
+  {name: 'Tom', jobTitle: 'Power peeper', boss: 'Babes'},
+  {name: 'Steve', jobTitle: 'Yes man', boss: 'Rahim'},
+]
+for (i in Friends){
+  if (Friends[i].boss) {
+  console.log(`${Friends[i].jobTitle} ${Friends[i].name} reports to ${Friends[i].boss}`);
+  }
+  else  console.log(`${Friends[i].jobTitle} ${Friends[i].name} doesn't report to anybody.`);
+  
+  //console.log(`${Friends[i].name} is a professional ${Friends[i].jobTitle}`);
+}

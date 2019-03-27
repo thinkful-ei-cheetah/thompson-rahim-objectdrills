69dda6687a780934417b94ccce08ebc8d473bbb2
rahmor <rahmor@yahoo.com>
2019-03-27 11:48:10 -0400

    add self-reference

--- objectdrills.js
+++ objectdrills.js
@@ -10,14 +10,26 @@ function createMyObject() {
 }
 
 function updateObject(obj) {
-  let foo= 'foo', bar='bar' 
-  bizz= 'bizz' 
-  bang='bang';
-  Object.assign(obj,{foo,bar,bizz,bang});
+  let foo = "foo",
+    bar = "bar";
+  bizz = "bizz";
+  bang = "bang";
+  Object.assign(obj, { foo, bar, bizz, bang });
   return obj;
 }
 
-
+function personMaker() {
+  var person = {
+    firstName: "Paul",
+    lastName: "Jones",
+    // replace `null` with a function that uses self reference to return
+    // full name
+    fullName: function() {
+      return `${fistName}${lastName}`;
+    }
+  };
+  return person;
+}
 
 /* From here down, you are not expected to 
    understand.... for now :)  
@@ -29,30 +41,35 @@ function updateObject(obj) {
 
 (function testUpdateObject() {
   var oldObj = {
-    cats: 'cats',
-    dogs: 'dogs',
+    cats: "cats",
+    dogs: "dogs"
   };
   var newObj = updateObject(oldObj);
-  if (typeof newObj !== 'object') { 
-    console.error('ERROR: `createMyObject` must return an object');
-    return false
-  }
-  ['foo', 'bar', 'bizz', 'bang'].forEach(function(key) {
-    if (!(key in newObj)) {
-      console.error('ERROR: `' + key + '` not in object returned by `updateObject`');
-      return false;
-    }
-  });
-  ['foo', 'bar', 'bizz', 'bang'].forEach(function(key) {
-    if (newObj[key] !== key) {
-      console.error('ERROR: `' + key + '` should be "' + key + '" but was ' + newObj[key]);
-      return false;
-    }
-  });
-  if (!(newObj.cats === 'cats' && newObj.dogs === 'dogs')) {
-    console.error('ERROR: your function doesn\'t preserve existing key/value pairs');
+  if (typeof newObj !== "object") {
+    console.error("ERROR: `createMyObject` must return an object");
     return false;
   }
-  console.log('SUCCESS: `updateObject` works correctly!');
-  
+  ["foo", "bar", "bizz", "bang"].forEach(function(key) {
+    if (!(key in newObj)) {
+      console.error(
+        "ERROR: `" + key + "` not in object returned by `updateObject`"
+      );
+      return false;
+    }
+  });
+  ["foo", "bar", "bizz", "bang"].forEach(function(key) {
+    if (newObj[key] !== key) {
+      console.error(
+        "ERROR: `" + key + '` should be "' + key + '" but was ' + newObj[key]
+      );
+      return false;
+    }
+  });
+  if (!(newObj.cats === "cats" && newObj.dogs === "dogs")) {
+    console.error(
+      "ERROR: your function doesn't preserve existing key/value pairs"
+    );
+    return false;
+  }
+  console.log("SUCCESS: `updateObject` works correctly!");
 })();

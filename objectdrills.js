f9cc7e469c6a05379725e61310ff4a4a8442a4b9
Thompson Galetovic <200conceptz@gmail.com>
2019-03-27 14:17:07 -0400

    up to lunch tweaks

--- objectdrills.js
+++ objectdrills.js
@@ -45,12 +45,9 @@ function keyDeleter(obj) {
 }
 
 function makeStudentsReport(data) {
-  // your code here
   let newArray = [];
-  data.forEach(function(data) {
-    newArray.push(`${data.name}: ${data.grade}`);
-  });
-  console.log(newArray);
+  data.forEach(data => newArray.push(`${data.name}: ${data.grade}`) );
+ // console.log(newArray);
   return newArray;
 }
 
@@ -63,7 +60,60 @@ function enrollInSummerSchool(students){
   
 }
 
-/*
+function findById(items, idNum){
+  for (i in items) {
+    if ( Object.is(items[i].id , idNum) ){   //method check
+//    if ( items[i].id === idNum ){         //alt check
+        return items[i];
+    }
+  }
+}
+
+
+
+
+/*          tests
+
+// you can pass in `scratchData` to test out `findByid`
+// your function
+const scratchData = [
+  { id: 22, foo: 'bar' },
+  { id: 28, foo: 'bizz' },
+  { id: 19, foo: 'bazz' },
+];
+
+
+function testIt() {
+  const testData = [
+    { id: 1, foo: 'bar' },
+    { id: 2, foo: 'bizz' },
+    { id: 3, bang: 'boo' },
+  ];
+  const result = findById(testData, 3);
+  if (!(result && result !== null && typeof result === 'object')) {
+    console.error('`findById` must return an object');
+    return;
+  }
+  if (result.id !== 3) {
+    console.error(
+      'Asked for item with id of `3` but got back one with id of ' + result.id
+    );
+    return;
+  }
+  if (result.bang !== 'boo') {
+    console.error(
+      'Expected all key/value pairs from target object to be returned'
+    );
+    return;
+  }
+  console.log('SUCCESS: `findByid` is working');
+}
+
+testIt();
+
+
+--------------------------------------------------------------------
+
 const studentData = [
   {
     name: 'Tim',

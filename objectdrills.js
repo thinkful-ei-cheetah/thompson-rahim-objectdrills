f2d2a4437ab29cedbec7ce1aeda73474460e6de2
Thompson Galetovic <200conceptz@gmail.com>
2019-03-27 12:29:15 -0400

    Enroll funciton

--- objectdrills.js
+++ objectdrills.js
@@ -44,6 +44,104 @@ function keyDeleter(obj) {
   return obj;
 }
 
+function makeStudentsReport(data) {
+  // your code here
+  let newArray = [];
+  data.forEach(function(data) {
+    newArray.push(`${data.name}: ${data.grade}`);
+  });
+  console.log(newArray);
+  return newArray;
+}
+
+
+function enrollInSummerSchool(students){
+  for (report in students){
+    students[report].status="In Summer school";
+  }
+  return students;
+  
+}
+
+/*
+const studentData = [
+  {
+    name: 'Tim',
+    status: 'Current student',
+    course: 'Biology',
+  },
+  {
+    name: 'Sue',
+    status: 'Withdrawn',
+    course: 'Mathematics',
+  },
+  {
+    name: 'Liz',
+    status: 'On leave',
+    course: 'Computer science',
+  },
+];
+
+
+
+function testIt() {
+  var testData = [
+    {
+      name: 'Burt',
+      status: 'Playing hooky',
+      course: 'Biology',
+    },
+    {
+      name: 'Melanie',
+      status: 'Sick',
+      course: 'Mathematics',
+    },
+    {
+      name: 'Leonard',
+      status: 'AWOL',
+      course: 'Computer science',
+    },
+  ];
+
+  var results = enrollInSummerSchool(testData);
+
+  if (!(results && results instanceof Array)) {
+    console.error('FAILURE: `enrollSummerSchool` must return an array');
+    return;
+  }
+  var result = testData.every(function(student) {
+    var match = results.find(function(_student) {
+      return (
+        _student.name === student.name &&
+        _student.course === student.course &&
+        _student.status.toLowerCase() === 'in summer school'
+      );
+    });
+    return match !== undefined;
+  });
+  if (!result) {
+    console.error(
+      'FAILURE: `enrollSummerSchool` should return ' +
+        'original key/value pairs for each student, and ' +
+        'update `status` to "In Summer school": ' +
+        JSON.stringify(results)
+    );
+  } else {
+    console.info('SUCCESS: `enrollSummerSchool` is working');
+  }
+}
+
+testIt();
+
+
+
+/* From here down, you are not expected to 
+   understand.... for now :)  
+   
+   Nothing to see here!
+   
+
+
 (function testKeyDeleter() {
   var obj = keyDeleter({
     foo: "foo",
@@ -71,24 +169,9 @@ function keyDeleter(obj) {
   console.log("SUCCESS: `keyDeleter` works correctly!");
 })();
 
-function makeStudentsReport(data) {
-  // your code here
-  let newArray = [];
-  data.forEach(function(data) {
-    newArray.push(`${data.name}: ${data.grade}`);
-  });
-  console.log(newArray);
-  return newArray;
-}
+----------------------------------------------------------------
 
-/* From here down, you are not expected to 
-   understand.... for now :)  
-   
-   Nothing to see here!
-   
-*/
-
-// tests
+// Data tests
 
 function testIt() {
   const testData = [
